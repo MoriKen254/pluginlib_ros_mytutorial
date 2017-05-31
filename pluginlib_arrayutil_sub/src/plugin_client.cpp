780207ac8f593a5b5a030a7502cd0c392ba473d1
MoriKen254 <masamorobo@gmail.com>
2017-05-31 21:39:51 +0900

    WIP clientのビルドを通した．

---
+++ pluginlib_arrayutil_sub/src/plugin_client.cpp
@@ -0,0 +1,12 @@
+#include <boost/shared_ptr.hpp>
+
+#include <pluginlib/class_loader.h>
+//#include "pluginlib_arrayutil_base/arrayutil_base.h"
+//#include "pluginlib_arrayutil_sub/sum.h"
+
+int main()
+{
+  //pluginlib::ClassLoader<arrayutil_base::ArrayUtil> arrayutil_loader("pluginlib_arrayutil_base", "ArrayUtil");
+
+  return 0;
+}
